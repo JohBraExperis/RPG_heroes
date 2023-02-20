2f54759a4187494ce6ca288f82e407901964f7cf
Johann Braaten <johann.braaten@no.experis.com>
2023-02-20 09:29:24 +0100

    chore: fixed some file structure errors

--- HeroTest/HeroTests.cs
+++ HeroTest/HeroTests.cs
@@ -1,10 +1,5 @@
 ﻿using RPG_heroes.Heroes;
 using RPG_heroes.Heroes.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeroTest
 {
