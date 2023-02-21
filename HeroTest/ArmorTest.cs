6262204699e78b3e8bb0376f4eca60c69a6f7624
Johann Braaten <johann.braaten@no.experis.com>
2023-02-21 11:06:05 +0100

    feat: added weaponTests. Chore: added some comments

--- HeroTest/ArmorTest.cs
+++ HeroTest/ArmorTest.cs
@@ -24,44 +24,56 @@ namespace HeroTest
         [Fact]
         public void Armor_RequiredLevel_ShouldBeCorrect()
         {
+            // Arrange
             Armor armor = new Armor("Test", 1, Slot.Body, ArmorType.Mail, new(2, 2, 2));
-
             int expected = 1;
+
+            //Act
             int actual = armor.RequiredLevel;
 
+            //Assert
             Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void Armor_Slot_ShouldBeCorrect()
         {
+            // Arrange
             Armor armor = new Armor("Test", 1, Slot.Body, ArmorType.Mail, new(2, 2, 2));
-
             Slot expected = Slot.Body;
+
+            //Act
             Slot actual = armor.EquipmentSlot;
 
+            //Assert
             Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void Armor_ArmorType_ShouldBeCorrect()
         {
+            // Arrange
             Armor armor = new Armor("Test", 1, Slot.Body, ArmorType.Mail, new(2, 2, 2));
-
             ArmorType expected = ArmorType.Mail;
+
+            //Act
             ArmorType actual = armor.ArmorTypes;
 
+            //Assert
             Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void Armor_ArmorAttribute_ShouldBeCorrect()
         {
+            // Arrange
             Armor armor = new Armor("Test", 1, Slot.Body, ArmorType.Mail, new(2, 2, 2));
-
             HeroAttributes expected = new(2, 2, 2);
+
+            //Act
             HeroAttributes actual = armor.ArmorAttribute;
 
+            //Assert
             Assert.Equivalent(expected, actual);
         }
 
---
+++ HeroTest/WeaponTest.cs
@@ -0,0 +1,85 @@
+﻿using RPG_heroes.Heroes.Attributes;
+using RPG_heroes.Heroes.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroTest
+{
+    public class WeaponTest
+    {
+
+        [Fact]
+        public void Weapon_Name_ShouldBeCorrect()
+        {
+            // Arange
+            Weapons weapon = new Weapons("Axe", 1, Slot.Weapon, 1, WeaponType.Axe);
+            string expected = "Axe";
+
+            //Act
+            string actual = weapon.Name;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Weapon_RequiredLevel_ShouldBeCorrect()
+        {
+            // Arange
+            Weapons weapon = new Weapons("Axe", 1, Slot.Weapon, 1, WeaponType.Axe);
+            int expected = 1;
+
+            //Act
+            int actual = weapon.RequiredLevel;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Weapon_Slot_ShouldBeCorrect()
+        {
+            // Arange
+            Weapons weapon = new Weapons("Axe", 1, Slot.Weapon, 1, WeaponType.Axe);
+            Slot expected = Slot.Weapon;
+
+            //Act
+            Slot actual = weapon.EquipmentSlot;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Weapon_WeaponDamage_ShouldBeCorrect()
+        {
+            // Arange
+            Weapons weapon = new Weapons("Axe", 1, Slot.Weapon, 1, WeaponType.Axe);
+            double expected = 1;
+
+            //Act
+            double actual = weapon.WeaponDamage;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Weapon_WeaponType_ShouldBeCorrect()
+        {
+            // Arange
+            Weapons weapon = new Weapons("Axe", 1, Slot.Weapon, 1, WeaponType.Axe);
+            WeaponType expected = WeaponType.Axe;
+
+            //Act
+            WeaponType actual = weapon.WeaponTypes;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
